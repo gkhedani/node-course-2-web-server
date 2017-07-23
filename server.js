9daf4c1d2c2495428bcff637a23c205bec3dc195
Gail Hedani <gailhedani@Gails-MacBook-Pro-2.local>
2017-07-22 15:37:17 -1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+hbs.registerPartials(__dirname + "/views/partials");
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamIt", (text) => {
+  return text.toUpperCase();
+})
+
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+  // next tells express when you're middleware is done
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log + "\n", (err) => {
+    if (err) {
+      console.log("Unable to append to server.log");
+    }
+  })
+  next();
+});
+
+// maintenance message middleware
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
+
+// fast way to setup a static website
+// __dirname stores the path of your directory
+app.use(express.static(__dirname + "/public"));
+
+// handler for the http requests
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello Express!</h1>");
+  // example for sending json data
+  // res.send({
+  //   name: "Gail",
+  //   likes: [
+  //     "Rurouni Kenshin",
+  //     "Hawaii"
+  //   ]
+  // });
+  res.render("home.hbs", {
+    pageTitle:  "My First Website",
+    welcomeMessage:  "Aloha!  Welcome to my website!"
+  })
+});
+
+
+app.get("/about", (req, res) => {
+  // res.send("About Page");
+  res.render("about.hbs", {
+    pageTitle: "ABOUT PAGE",
+  });
+});
+
+// when the app fails
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Error!"
+  });
+});
+
+// bind the application to a port on a machine
+app.listen(3000, () => {
+  console.log("Server is up on port 3000");
+});
